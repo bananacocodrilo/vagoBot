599873b14134ac0534561f8d8ed0dc05d58afe7a
antonio <alors@sycket.com>
2017-03-26 21:01:28 +0200

    testing code split

--- src/bot_logic.js
+++ src/bot_logic.js
@@ -1,1 +1,42 @@
+const Yeelight = require('yeelight2');
 
+let bot = new TelegramBot(token, { polling: true });
+let lights = {};
+
+Yeelight.discover(function(light) {
+    let key = light.name ? light.name : light.id;
+    lights[key] = light;
+    console.log('New bulb found:', key);
+
+});
+
+
+module.exports = function() {
+
+    function toggle(msg) {
+
+        let response = 'Toggle: ';
+        let list = msg.text.substr(8).split(' ');
+
+
+        console.log(list.length);
+        console.log(list);
+
+        if (!list[0].length) {
+            list = Object.keys(lights);
+            // console.log('entra');
+            // console.log(list);
+
+        }
+
+        list.forEach(function(bulb) {
+            if (lights.hasOwnProperty(bulb)) {
+                lights[bulb].toggle();
+                response += bulb + ' ';
+            }
+        });
+
+        return response;
+    }
+
+}
--- src/main.js
+++ src/main.js
@@ -1,52 +1,11 @@
-const Yeelight = require('yeelight2');
 const TelegramBot = require('node-telegram-bot-api');
-
 let token = '279911010:AAHehLfG-zmftpKCs4ctZtsIe8CB6wbG09M';
-let lights = {};
-var bot = new TelegramBot(token, { polling: true });
-
-Yeelight.discover(function(light) {
-    let key = light.name ? light.name : light.id;
-    lights[key] = light;
-    console.log('New bulb found:', key);
-
-});
-
-
+let logic = require("./bot_logic.js");
 
 bot.onText(/\/toggle()/, function(msg, match) {
     let chatId = msg.chat.id;
-    let response = 'Toggle: ';
-    let list = msg.text.substr(8).split(' ');
-
-
-    console.log(list.length);
-    console.log(list);
-
-    if (!list[0].length) {
-        list = Object.keys(lights);
-        console.log('entra');
-        console.log(list);
-
-    }
-
-
-    // else {
-    //     for (var bulb in lights) {
-
-    //         if (lights.hasOwnProperty(bulb)) {
-    //             lights[bulb].toggle();
-    //             response += bulb + ' ';
-    //         }
-    //     }
-    // }
-    list.forEach(function(bulb) {
-        if (lights.hasOwnProperty(bulb)) {
-            lights[bulb].toggle();
-            response += bulb + ' ';
-        }
-    });
-
+    let response = logic.toggle(msg);
 
     bot.sendMessage(chatId, response);
+
 });
